9161e3e533d418578d34529ab257edd382c7e181
Carlos Andre Couto <carlosandrecouto@live.com>
2021-08-15 18:56:26 -0300

    improvement(view_pdf_screen): implementa tela

--- lib/features/store/presentation/ui/screens/purchase_confirmation.dart
+++ lib/features/store/presentation/ui/screens/purchase_confirmation.dart
@@ -1,14 +1,14 @@
-import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_full_pdf_viewer/flutter_full_pdf_viewer.dart';
-import 'package:flutter_html_to_pdf/flutter_html_to_pdf.dart';
 import 'package:framework/core/ui/constants/app_colors.dart';
 import 'package:framework/core/ui/constants/app_dimens.dart';
 import 'package:framework/core/ui/constants/app_styles.dart';
 import 'package:framework/core/ui/widgets/custom_app_bar.dart';
+import 'package:framework/features/store/presentation/ui/screens/view_pdf_screen.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pdfLib;
 
 class PurchaseConfirmationScreen extends StatefulWidget {
   final String purchaseValue;
@@ -29,43 +29,47 @@ class _PurchaseConfirmationScreenState extends State<PurchaseConfirmationScreen>
   @override
   void initState() {
     super.initState();
-    templateHTML();
   }
 
-  Future<void> templateHTML() async {
-    final htmlContent = """
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <style>
-          html, body {
-            width: 100%;
-          }
-        </style>
-      </head>
-      <body>
-       <div style="width: 100%; display: flex; flex-direction: column;">
-          <h2 style="font-size:20px; color: #5f3473;">Detalhes da compra</h2>
-        
-          <ul>
-            <li><p style="font-size:16px;"><strong>Total do pedido: </strong> ${widget.purchaseValue}</p></li>
-            <li><p style="font-size:16px;"><strong>Produtos do pedido: </strong> ${widget.purchaseProducts}</p></li>
-          </ul>
-  
-          <div style="display:flex; flex-wrap:wrap; justify-content:center; margin-top:50px;">
-            <img style="width:100px; height:100px; object-fit:contain;" src="https://media-exp1.licdn.com/dms/image/C560BAQEpMK9fjp29IA/company-logo_200_200/0/1519866108006?e=2159024400&v=beta&t=tH705_pZXOlqZGN9lqPaRUsmF6mxQVsAwxzGbSBG03E">
-          </div>
-        </div>
-      </body>
-    </html>
-    """;
+  _creatPdf({contex, purchaseProducts, purchaseValue}) async {
+    final pdfLib.Document pdf = pdfLib.Document(deflate: zlib.encode);
 
-    Directory appDocDir = await getApplicationDocumentsDirectory();
-    final targetPath = appDocDir.path;
-    final targetFileName = "example-pdf";
+    pdf.addPage(pdfLib.MultiPage(
+        build: (context) => [
+              pdfLib.Column(
+                crossAxisAlignment: pdfLib.CrossAxisAlignment.start,
+                mainAxisSize: pdfLib.MainAxisSize.max,
+                children: [
+                  pdfLib.Center(
+                    child: pdfLib.Text(
+                      'Comprovante de pagamento',
+                      style: pdfLib.TextStyle(fontSize: 18, color: PdfColor.fromInt(0xFF5f3473)),
+                    ),
+                  ),
+                  pdfLib.SizedBox(height: AppDimens.large),
+                  pdfLib.Text(
+                    'Resumo do pedido:',
+                    style: pdfLib.TextStyle(fontSize: 14),
+                  ),
+                ],
+              ),
+              pdfLib.SizedBox(height: AppDimens.small),
+              pdfLib.Table.fromTextArray(data: <List<String>>[
+                <String>['Produtos', 'Valor total'],
+                [purchaseProducts, purchaseValue]
+              ])
+            ]));
 
-    final generatedPdfFile = await FlutterHtmlToPdf.convertFromHtmlContent(htmlContent, targetPath, targetFileName);
-    generatedPdfFilePath = generatedPdfFile.path;
+    final String dir = (await getApplicationDocumentsDirectory()).path;
+
+    final String path = '$dir/pdfExample.pdf';
+    final File file = File(path);
+    file.writeAsBytesSync(pdf.save());
+
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => ViewPdfScreen(
+              path,
+            )));
   }
 
   @override
@@ -94,15 +98,10 @@ class _PurchaseConfirmationScreenState extends State<PurchaseConfirmationScreen>
                 style: AppStyles.regular12().copyWith(color: AppColors.white_900),
               ),
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => PDFViewerScaffold(
-                            appBar: CustomAppBar(
-                              title: "Comprovante de pagamento",
-                            ),
-                            path: generatedPdfFilePath,
-                          )),
+                _creatPdf(
+                  contex: context,
+                  purchaseProducts: widget.purchaseProducts,
+                  purchaseValue: widget.purchaseValue,
                 );
               },
             ),
---
+++ lib/features/store/presentation/ui/screens/view_pdf_screen.dart
@@ -0,0 +1,56 @@
+import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
+import 'package:flutter/material.dart';
+import 'package:framework/core/ui/constants/app_colors.dart';
+import 'package:share_extend/share_extend.dart';
+
+class ViewPdfScreen extends StatefulWidget {
+  ViewPdfScreen(this.path);
+
+  final String path;
+
+  @override
+  _ViewPdfScreenState createState() => _ViewPdfScreenState();
+}
+
+class _ViewPdfScreenState extends State<ViewPdfScreen> {
+  PDFDocument _doc;
+  bool _loading;
+
+  @override
+  void initState() {
+    super.initState();
+    initPdf();
+  }
+
+  initPdf() async {
+    setState(() {
+      _loading = true;
+    });
+    final doc = await PDFDocument.fromAsset(widget.path);
+    setState(() {
+      _doc = doc;
+      _loading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Comprovante de pagamento'),
+          actions: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 5),
+              alignment: Alignment.center,
+              child: IconButton(
+                  icon: Icon(Icons.share, color: AppColors.white_900),
+                  iconSize: 30,
+                  onPressed: () {
+                    ShareExtend.share(widget.path, "file", sharePanelTitle: "Compartilhar PDF", subject: "example-pdf");
+                  }),
+            )
+          ],
+        ),
+        body: _loading ? Center(child: CircularProgressIndicator()) : PDFViewer(document: _doc));
+  }
+}
